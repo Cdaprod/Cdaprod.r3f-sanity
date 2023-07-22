9ee4855a82b17acbeb7651e1132f7e12112477e7
David <104325852+Cdaprod@users.noreply.github.com>
2023-07-22 20:21:49 +0000

    init

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default function App() {
       <hemisphereLight groundColor="red" />
       <Float floatIntensity={10} rotationIntensity={4}>
         <Html castShadow receiveShadow occlude="blending" transform>
-          <iframe title="embed" width={700} height={500} src="https://threejs.org/" frameBorder={0} />
+          <iframe title="embed" width={700} height={500} src="https://sanity.cdaprod.dev" frameBorder={0} />
         </Html>
       </Float>
       <Geometries />
