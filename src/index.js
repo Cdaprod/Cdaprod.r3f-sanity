009559cf6b55b3fa040bab64f73cd1e47a6851ae
David <104325852+Cdaprod@users.noreply.github.com>
2023-07-22 22:02:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom';
 import React from 'react';
-import './styles.css';
+import './styles/styles.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
